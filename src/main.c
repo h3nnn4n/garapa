8efafea3947b43c2164577dcee561bf4c4e50e27
h3nnn4n <uber.renan@gmail.com>
2016-12-08 21:55:46 +0000

    Patched the code to run 8080PRE.COM by default

--- src/main.c
+++ src/main.c
@@ -13,17 +13,17 @@
 int main(int argc, char *argv[]) {
     FILE *f = NULL;
     _cpu_info cpu;
+    off_t buffer_size = -1;
 
     if ( argc == 1 ) {
         printf("Usage: %s file\n", argv[0]);
         return EXIT_FAILURE;
     } else {
         init_cpu(&cpu);
-        off_t buffer_size = -1;
         f = fopen(argv[1], "rb");
         buffer_size = fsize( argv[1] );
 
-        if ( fread(cpu.memory, buffer_size, 1, f) != 1 ) {
+        if ( fread(cpu.memory + 0x100, buffer_size, 1, f) != 1 ) {
             printf("Something went weird while reding into buffer\n");
         }
 
@@ -35,27 +35,12 @@ int main(int argc, char *argv[]) {
     sdl_init();
     atexit(SDL_Quit);
 
+    cpu.pc = 0x100;
+    cpu.sp = 0x1000;
+
     while ( 1 ) {
         /*cpu.pc += disassembler(cpu.memory, cpu.pc);*/
         emulator ( &cpu );
-
-        if ( cpu.instructions_executed % 100000 == 0 ) {
-            printf(" -- %8d\n", cpu.instructions_executed);
-        }
-
-
-        if ( cpu.interrupt_addr == 0x10 && cpu.cycles > 28500 ) {
-            cpu.cycles -= 28500;
-            cpu.interrupt_addr = 0x08;
-            emulate_INTERRUPT( &cpu );
-            update_screen ( &cpu );
-        } else if ( cpu.interrupt_addr == 0x08 && cpu.cycles > 4850 ) {
-            cpu.cycles -= 4850;
-            cpu.interrupt_addr = 0x10;
-            emulate_INTERRUPT( &cpu );
-            update_input ( &cpu );
-            /*SDL_Delay(16);*/
-        }
     }
 
     return 0;
