cf53f9bde2984650b100abcffbbbb19089f45879
h3nnn4n <uber.renan@gmail.com>
2016-12-09 00:49:18 +0000

    Several debuging leftover code

--- src/main.c
+++ src/main.c
@@ -13,17 +13,19 @@
 int main(int argc, char *argv[]) {
     FILE *f = NULL;
     _cpu_info cpu;
-    off_t buffer_size = -1;
 
     if ( argc == 1 ) {
         printf("Usage: %s file\n", argv[0]);
         return EXIT_FAILURE;
     } else {
         init_cpu(&cpu);
+        off_t buffer_size = -1;
         f = fopen(argv[1], "rb");
         buffer_size = fsize( argv[1] );
 
-        if ( fread(cpu.memory + 0x100, buffer_size, 1, f) != 1 ) {
+        uint16_t offset = 0x000;
+
+        if ( fread(cpu.memory + offset, buffer_size, 1, f) != 1 ) {
             printf("Something went weird while reding into buffer\n");
         }
 
@@ -35,12 +37,55 @@ int main(int argc, char *argv[]) {
     sdl_init();
     atexit(SDL_Quit);
 
-    cpu.pc = 0x100;
-    cpu.sp = 0x1000;
+    /*int ff = 0;*/
 
     while ( 1 ) {
         /*cpu.pc += disassembler(cpu.memory, cpu.pc);*/
+
+        /*if ( cpu.memory[cpu.pc+0] == 0xcd &&*/
+             /*cpu.memory[cpu.pc+1] == 0x50 &&*/
+             /*cpu.memory[cpu.pc+2] == 0x05 ){*/
+            /*ff = 1;*/
+            /*printf("%04x -> sp = %04x\n", cpu.pc, cpu.sp);// %4x: %2x   \t %4x: %2x\n", cpu.pc, 0x2078-1, cpu.memory[0x2078-1], 0x2078-2, cpu.memory[0x2078-2]);*/
+        /*}*/
+
         emulator ( &cpu );
+
+        /*if ( cpu.instructions_executed > 2030000 ) {*/
+            /*disassembler ( cpu.memory, cpu.pc );*/
+            /*print_registers(&cpu);*/
+        /*}*/
+
+        /*if ( cpu.instructions_executed > 2040000 ) {*/
+            /*break;*/
+        /*}*/
+
+
+        /*if ( ff ) {*/
+            /*printf("%04x -> sp = %04x\n", cpu.pc, cpu.sp);// %4x: %2x   \t %4x: %2x\n", cpu.pc, 0x2078-1, cpu.memory[0x2078-1], 0x2078-2, cpu.memory[0x2078-2]);*/
+            /*for (int i = 0; i < 10; ++i) {*/
+                /*printf("        %4x: %2x\n", 0x23e8 + (i-4), cpu.memory[0x23e8 + (i-4)]);*/
+            /*}*/
+            /*break;*/
+        /*}*/
+
+        /*if ( cpu.instructions_executed % 100000 == 0 ) {*/
+            /*printf(" -- %8d\n", cpu.instructions_executed);*/
+        /*}*/
+
+
+        if ( cpu.interrupt_addr == 0x10 && cpu.cycles > 28500 ) {
+            cpu.cycles -= 28500;
+            cpu.interrupt_addr = 0x08;
+            emulate_INTERRUPT( &cpu );
+            update_screen ( &cpu );
+        } else if ( cpu.interrupt_addr == 0x08 && cpu.cycles > 4850 ) {
+            cpu.cycles -= 4850;
+            cpu.interrupt_addr = 0x10;
+            emulate_INTERRUPT( &cpu );
+            update_input ( &cpu );
+            /*SDL_Delay(16);*/
+        }
     }
 
     return 0;
