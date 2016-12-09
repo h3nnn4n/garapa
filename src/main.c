1c3aa5fdd9ea6b4e2958b91eb2c169b4be9e7bd2
h3nnn4n <uber.renan@gmail.com>
2016-12-09 13:18:58 +0000

    Hardcoded some stuff to run 8080PRE.COM

--- src/main.c
+++ src/main.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[]) {
         f = fopen(argv[1], "rb");
         buffer_size = fsize( argv[1] );
 
-        uint16_t offset = 0x000;
+        uint16_t offset = 0x100;
 
         if ( fread(cpu.memory + offset, buffer_size, 1, f) != 1 ) {
             printf("Something went weird while reding into buffer\n");
@@ -31,60 +31,48 @@ int main(int argc, char *argv[]) {
 
         fclose(f);
 
-        printf("Opening: %s\nFile contains: %lu bytes\n", argv[1], buffer_size);
+        /*printf("Opening: %s\nFile contains: %lu bytes\n", argv[1], buffer_size);*/
     }
 
     sdl_init();
     atexit(SDL_Quit);
 
-    /*int ff = 0;*/
+    cpu.sp = 0x0;
+    cpu.pc = 0x100;
+
+    cpu.memory[0x05] = 0xC9;
+
+    int success = 0;
 
     while ( 1 ) {
         /*cpu.pc += disassembler(cpu.memory, cpu.pc);*/
 
-        /*if ( cpu.memory[cpu.pc+0] == 0xcd &&*/
-             /*cpu.memory[cpu.pc+1] == 0x50 &&*/
-             /*cpu.memory[cpu.pc+2] == 0x05 ){*/
-            /*ff = 1;*/
-            /*printf("%04x -> sp = %04x\n", cpu.pc, cpu.sp);// %4x: %2x   \t %4x: %2x\n", cpu.pc, 0x2078-1, cpu.memory[0x2078-1], 0x2078-2, cpu.memory[0x2078-2]);*/
-        /*}*/
+        if (cpu.pc == 0x0005) {
+            if (cpu.c == 9) {
+                int i;
+                for (i = cpu.d << 8 | cpu.e; cpu.memory[i] != '$'; ++i)
+                    printf("%c", cpu.memory[i]);
+                success = 1;
+            }
+            if (cpu.c == 2)
+                putchar((char)cpu.e);
+        }
 
         emulator ( &cpu );
 
-        /*if ( cpu.instructions_executed > 2030000 ) {*/
-            /*disassembler ( cpu.memory, cpu.pc );*/
-            /*print_registers(&cpu);*/
-        /*}*/
+        if (cpu.memory[cpu.pc] == 0x76) {
+            printf("HLT at %04X\n", cpu.pc);
+            exit(1);
+        }
 
-        /*if ( cpu.instructions_executed > 2040000 ) {*/
-            /*break;*/
-        /*}*/
+        if ( cpu.pc == 0x0000 && !success ) {
+            printf("Got to 0x0000 without the success flag. RIP\n");
+            exit(-1);
+        }
 
-
-        /*if ( ff ) {*/
-            /*printf("%04x -> sp = %04x\n", cpu.pc, cpu.sp);// %4x: %2x   \t %4x: %2x\n", cpu.pc, 0x2078-1, cpu.memory[0x2078-1], 0x2078-2, cpu.memory[0x2078-2]);*/
-            /*for (int i = 0; i < 10; ++i) {*/
-                /*printf("        %4x: %2x\n", 0x23e8 + (i-4), cpu.memory[0x23e8 + (i-4)]);*/
-            /*}*/
-            /*break;*/
-        /*}*/
-
-        /*if ( cpu.instructions_executed % 100000 == 0 ) {*/
-            /*printf(" -- %8d\n", cpu.instructions_executed);*/
-        /*}*/
-
-
-        if ( cpu.interrupt_addr == 0x10 && cpu.cycles > 28500 ) {
-            cpu.cycles -= 28500;
-            cpu.interrupt_addr = 0x08;
-            emulate_INTERRUPT( &cpu );
-            update_screen ( &cpu );
-        } else if ( cpu.interrupt_addr == 0x08 && cpu.cycles > 4850 ) {
-            cpu.cycles -= 4850;
-            cpu.interrupt_addr = 0x10;
-            emulate_INTERRUPT( &cpu );
-            update_input ( &cpu );
-            /*SDL_Delay(16);*/
+        if (cpu.pc == 0) {
+            printf("\n\n\n");
+            break;;
         }
     }
 
