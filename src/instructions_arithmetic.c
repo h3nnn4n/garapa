5800eecbac2dec5342784f80f225d6a8884c3430
h3nnn4n <uber.renan@gmail.com>
2017-01-03 15:29:33 +0000

    Now passes blargg's mem_timing

--- src/instructions_arithmetic.c
+++ src/instructions_arithmetic.c
@@ -330,8 +330,11 @@ void emulate_INR ( _cpu_info *cpu ) {
             cpu->flags.h  = (answer & 0x0f) == 0x00;
             break;
         case 0x34: // INR M
-            cpu->mem_controller.memory[cpu->h << 8 | cpu->l] += 1;
-            answer = cpu->mem_controller.memory[cpu->h << 8 | cpu->l];
+            answer = read_byte_with_tick ( cpu, read_hl ( cpu ) );
+            answer += 1;
+            write_byte_with_tick ( cpu, read_hl ( cpu ), answer );
+            /*cpu->mem_controller.memory[cpu->h << 8 | cpu->l] += 1;*/
+            /*answer = cpu->mem_controller.memory[cpu->h << 8 | cpu->l];*/
             cpu->flags.h  = (answer & 0x0f) == 0x00;
             break;
         case 0x3c: // INR A
