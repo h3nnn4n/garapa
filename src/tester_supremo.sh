ef099810100c65b635147c8c8e2da045bd8ccbd2
h3nnn4n <uber.renan@gmail.com>
2017-05-21 01:09:19 +0000

    Now prints out the commands and added ALL

--- src/tester_supremo.sh
+++ src/tester_supremo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TESTF=tests_folder
-TESTS="FBDP NDP KBR CMA HA LELmark"
+TESTS="FBDP NDP KBR CMA HA LELmark ALL"
 
 if [[ ! -d "$TESTF" ]]; then
     mkdir $TESTF
@@ -12,17 +12,18 @@ for name in $TESTS; do
     path=$TESTF/$name
     if [[ ! -d "$path" ]]; then
         mkdir $path
-        echo Created $path
+        echo mkdir $path
     fi
 
     make clean -s
     cmd="make -j -s CFLAGS=-D$name"
     $cmd 2> /dev/null
+    echo $cmd
 
     mv main "main_$name"
-    echo "main_$name" compiled
 
     now=$(date "+%d_%m_%H_%M_%S")
     (./"main_$name" tetris.gb > $path/$now) & disown %
+    echo "./"main_$name" tetris.gb > $path/$now"
 done
 
