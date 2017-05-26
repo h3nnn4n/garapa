528340cbe30939e4ad65bca15e4e31cdfb9a7ace
h3nnn4n <uber.renan@gmail.com>
2017-05-26 12:33:58 +0000

    Gave the correct address range for the APU

--- src/memory.c
+++ src/memory.c
@@ -74,8 +74,7 @@ uint8_t read_byte ( _cpu_info *cpu, uint16_t addr ) {
         }
     }
 
-    if ( (addr >= 0xff10 && addr <= 0xff26) ||
-         (addr >= 0xff30 && addr <= 0xff3f) ) {
+    if ( (addr >= 0xff10 && addr <= 0xff3f) ) {
         return apu_read_byte ( cpu, addr );
     }
 
@@ -261,8 +260,7 @@ void write_byte ( _cpu_info *cpu, uint16_t addr, uint8_t data ) {
         cartridge_write ( cpu, addr, data );
     }
 
-    if ( (addr >= 0xff10 && addr <= 0xff26) ||
-         (addr >= 0xff30 && addr <= 0xff3f) ) {
+    if ( (addr >= 0xff10 && addr <= 0xff3f) ) {
         apu_write_byte ( cpu, addr, data );
     }
 
