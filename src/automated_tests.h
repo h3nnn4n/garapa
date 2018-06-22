82bd752b2f55d621e9105e6db2b14f47cc26f3bf
h3nnn4n <uber.renan@gmail.com>
2018-06-22 15:46:53 -0300

    Fixed a very very very old typo

--- src/automated_tests.h
+++ src/automated_tests.h
@@ -17,7 +17,7 @@
  *    misrepresented as being the original software.                          *
  * 3. This notice may not be removed or altered from any source distribution. *
  ******************************************************************************/
-#ifndef AUTOMATED_TESTS_Hj
+#ifndef AUTOMATED_TESTS_H
 #define AUTOMATED_TESTS_H
 
 typedef struct {
