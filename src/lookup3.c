da90a92be3ad548877bd84dfb3784fc1c4b62c9f
h3nnn4n <uber.renan@gmail.com>
2020-11-13 07:33:36 -0300

    Use clang format to make it happily compile on macos

--- src/lookup3.c
+++ src/lookup3.c
@@ -44,7 +44,7 @@ on 1 byte), but shoehorning those bytes into integers efficiently is messy.
 #endif
 
 #pragma GCC diagnostic push
-#pragma GCC diagnostic ignored "-Wimplicit-fallthrough="
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
 
 /*
  * My best guess at if you are big-endian or little-endian.  This may
